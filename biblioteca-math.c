565b266371df6a6d5f236d9f84a5a834626bf49c
Rian Cesar Oliveira Souza <145462146+riancesaros@users.noreply.github.com>
2023-10-04 08:41:34 -0300

    Add files via upload
    Calculos de potenciação e raiz quadrada na linguagem C.  AINDA EM DESENVOLVIMENTO!

---
+++ biblioteca-math.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+#include <math.h>
+
+int main () {
+    int number1; number2, type;
+    
+    printf("Olá, bem vindo a calculadora de Potenciação e Raiz quadrada.");
+    printf("\nPara usar a função de potencia digite 'p' \nou digite 'r' para usar raiz quadrada.");
+    scanf ("%i", &type);
+    
+    if (type == 'p' || 'P') {
+             
+             
+    }
+    else if (type == 'r' || 'R') {
+             
+             
+    }
+    else {
+         
+    }
+}
